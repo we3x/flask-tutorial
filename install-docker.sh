bfe2119a9f4f7f185ef657e5dee9e7ae2b286a27
Goran Mekić <meka@lugons.org>
2016-01-22 16:18:01 +0100

    Barebones bootstrap

--- install-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp /vagrant/jessie-backports.list /etc/apt/sources.list.d
-apt-get update
-apt-get upgrade -y
-apt-get install -y docker.io
-apt-get clean -y
-docker pull python:2-onbuild
