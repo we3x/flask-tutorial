edc73d60f57618a6d40d617f08282b15924857dc
Goran Mekić <meka@lugons.org>
2016-01-22 16:40:38 +0100

    Add peewee

--- manage.py
+++ manage.py
@@ -2,10 +2,36 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from peewee import SqliteDatabase, Model, CharField, create_model_tables
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+db = SqliteDatabase('data.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class User(BaseModel):
+    username = CharField
+
+
+@app.before_request
+def db_connect():
+    db.connect()
+
+
+@app.teardown_request
+def db_close(exc):
+    if not db.is_closed():
+        db.close()
+
+
+models = [User]
+create_model_tables(models, fail_silently=True)
 
 
 @app.route('/')
