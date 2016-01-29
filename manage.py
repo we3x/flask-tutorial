6c14fb8462004c7a8b2c0f7a8e4326e7316f2b88
Goran Mekić <meka@lugons.org>
2016-01-29 11:14:26 +0100

    Add flask admin and template variables

--- manage.py
+++ manage.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template
+from flask_admin import Admin
+from flask_admin.contrib.peewee import ModelView
 from flask_bootstrap import Bootstrap
-from peewee import SqliteDatabase, Model, CharField, create_model_tables
+from peewee import (
+    SqliteDatabase,
+    Model,
+    CharField,
+    create_model_tables,
+)
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'iP4Yey6wd9su7cWtcS2urH0h1qWRCmP7Quakzg04k616wzAy4'
 bootstrap = Bootstrap(app)
 db = SqliteDatabase('data.db')
+admin = Admin(app, name='tilda', template_mode='bootstrap3')
 
 
 class BaseModel(Model):
@@ -16,7 +25,8 @@ class BaseModel(Model):
 
 
 class User(BaseModel):
-    username = CharField
+    username = CharField()
+    email = CharField()
 
 
 @app.before_request
@@ -33,10 +43,14 @@ def db_close(exc):
 models = [User]
 create_model_tables(models, fail_silently=True)
 
+for model in models:
+    admin.add_view(ModelView(model))
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    user_in_python = User.get(username='meka')
+    return render_template('index.html', user_in_template=user_in_python)
 
 
 if __name__ == '__main__':
